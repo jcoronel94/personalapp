9fa13acf6f7eb534517aadc08e6733616359b7f8
unknown <jcoronel94@gmail.com>
2021-05-10 17:41:15 -0400

    flexbox changes

--- src/pages/BlogPostList.vue
+++ src/pages/BlogPostList.vue
@@ -4,15 +4,13 @@
       <!-- Create `v-for` and apply a `key` for Vue. Here we are using a combination of the slug and index. -->
 
       <div class="md-layout md-gutter md-alignment-left">
-        <div    v-for="(post, index) in posts"
-          :key="post.slug + '_' + index" class ="md-layout-item  md-medium-size-50 md-small-size-100">
-        <md-card>
-          <router-link :to="'/blogs/' + post.slug">
-            <md-card-header>
-              <md-card-header-text>
-                <div class="md-title">{{ post.title }}</div>
-                <div class="md-subhead">{{ post.summary }}</div>
-              </md-card-header-text>
+        <div
+          v-for="(post, index) in posts"
+          :key="post.slug + '_' + index"
+          class="md-layout-item md-medium-size-50 md-small-size-100"
+        >
+          <md-card>
+            <router-link :to="'/blogs/' + post.slug">
               <md-card-media>
                 <img
                   v-if="post.featured_image"
@@ -21,9 +19,16 @@
                 />
                 <img v-else src="http://via.placeholder.com/250x250" alt="" />
               </md-card-media>
-            </md-card-header>
-          </router-link>
-        </md-card>
+              <md-card-header>
+                <md-card-header-text>
+                  <div class="md-title">{{ post.title }}</div>
+                </md-card-header-text>
+              </md-card-header>
+              <md-card-content>
+                {{ post.summary }}
+              </md-card-content>
+            </router-link>
+          </md-card>
         </div>
       </div>
     </div>
@@ -59,22 +64,27 @@ export default {
 
 <style lang="scss" scoped>
 .md-card {
-  min-height:150px;
-  margin:4px;
-  width:100%;
+  min-height: 150px;
+  margin: 4px;
+  width: 100%;
+  height: 95%;
   display: inline-block;
   vertical-align: top;
+  .md-card-media{
+    border-bottom:2px gainsboro solid;
+    padding-bottom:10
+    px;
+  }
 }
 
 .md-layout-item {
-    margin-top: 8px;
-    margin-bottom: 8px;
-
-    &:after {
-      width: 100%;
-      height: 100%;
-      display: none;
-      content: " ";
-    }
+  margin-top: 8px;
+  margin-bottom: 8px;
+  &:after {
+    width: 100%;
+    height: 100%;
+    display: none;
+    content: " ";
+  }
 }
 </style>
